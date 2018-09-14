1a8d6f0f715cb7527ddddaeedd9c85aadc23ce3d
jpq <you@example.com>
2018-09-14 14:24:21 +0800

    the first time commit

---
+++ hellogithub.c
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hellogithub.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2018年09月14日 14时22分14秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+int main()
+{
+	printf("github");
+}
